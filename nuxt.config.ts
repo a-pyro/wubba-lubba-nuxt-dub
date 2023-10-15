7677a088e58730208a47a901d1a4c828a0351baa
Ardi Germenji <ardi.germenji@interlogica.it>
2023-10-15 13:01:34 +0200

    chore(app): projects init and first setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    strict: true,
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: 'https://rickandmortyapi.com/api',
+    },
+  },
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Wubba Lubba Nuxt-Dub',
+      meta: [
+        {
+          name: 'description',
+          content: 'Rick and Morty Nuxt app',
+        },
+      ],
+    },
+    pageTransition: { name: 'page', mode: 'out-in' },
+  },
 })
